2dc5b8b6b31d026e6cf4549cf4da96eee7a27639
Gustavo Oliveira <guga.oliveira91@hotmail.com>
2017-02-21 11:49:17 -0300

    Update fileManip.py

--- File IO/arquivos/fileManip.py
+++ File IO/arquivos/fileManip.py
@@ -3,7 +3,7 @@ Created on 28 de jan de 2017
 
 @author: Gustavo
 '''
-arq=open("C:\\Users\\Gustavo\\Desktop\\v2.txt", "w+")
+arq=open("C:\\Users\\Gustavo\\Desktop\\v.txt", "w+")
 
 arq.write("Who wants to be king")
 print(arq.closed)
